742dc59bc03fd99fc006a5736bfe266b0bc40658
Zaiboot <zaiboot@gmail.com>
2015-06-18 14:52:49 -0600

    Adding missing unit tests

---
+++ Domain.Business.Test/ManagerBaseTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Trintech.Cadency.Utility.Exceptions;
+
+
+namespace Domain.Business.Test
+{
+    [ExcludeFromCodeCoverage]
+    public class ManagerBaseTss : ManagerBase
+    {
+        public ManagerBaseTss(ITrintechExceptionManager exceptionManager) : base(exceptionManager)
+        {
+        }
+    }
+    [TestClass]
+    [ExcludeFromCodeCoverage]
+    public class ManagerBaseTests : BaseManagerTester
+    {
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void ExceptionManagerNullTests()
+        {
+            new ManagerBaseTss(null);
+        }
+    }
+}
---
+++ Domain.Models.Tests/EntityTests.cs
@@ -0,0 +1,27 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Domain.Models.Tests
+{
+    [TestClass]
+    [ExcludeFromCodeCoverage]
+    public class EntityTests
+    {
+        [TestMethod]
+        public void CompareTest()
+        {
+            var entity = new EntitityOldVersion.Entity
+            {
+                Number = "number"
+            };
+            var entity2 = new EntitityOldVersion.Entity
+            {
+                Number = "number"
+            };
+            var compareResult = entity.CompareTo(entity2);
+            Assert.AreEqual(compareResult, 0);
+
+
+        }
+    }
+}
